f053293cffb62fa5486e9f34428d6e04845d4ee5
Amandeep Singh <am418470@dal.ca>
2024-09-30 18:40:48 -0700

    add recoil

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,10 @@
-import { PrismaClient } from '@repo/db/client';
+'use client';
+// import { PrismaClient } from '@repo/db/client';
+import { useBalance } from '@repo/store/hooks/useBalance';
 
-const client = new PrismaClient();
+// const client = new PrismaClient();
 
 export default function Home() {
-  return <div className="text-2xl">hi there</div>;
+  const balance = useBalance();
+  return <div className="text-2xl">hi there {balance}</div>;
 }
